f137b6e8709c76aaad1c6347ed07500af4862f7a
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-10-02 12:50:06 +0800

    Updates

---
+++ precache-manifest.aaa913c5d96c6203d92fd4a7906e2df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd50b831529af4f3b9ba27aa96c364c",
+    "url": "/MonsterRolodexTutDeploy/index.html"
+  },
+  {
+    "revision": "c72bdc6480e382168b33",
+    "url": "/MonsterRolodexTutDeploy/static/css/main.135d7494.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/MonsterRolodexTutDeploy/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRolodexTutDeploy/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c72bdc6480e382168b33",
+    "url": "/MonsterRolodexTutDeploy/static/js/main.0b04e182.chunk.js"
+  },
+  {
+    "revision": "4beeea0de3d905f0bfe3",
+    "url": "/MonsterRolodexTutDeploy/static/js/runtime-main.3584f4c7.js"
+  }
+]);
